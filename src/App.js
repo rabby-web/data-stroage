7b1f29ea08729282ab0f73416a56c1b05ef797fb
Rabby <rabby.webdeveloper@gmail.com>
2023-03-16 12:27:29 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Shoes from './components/Shoes/Shoes';
 function App() {
   return (
     <div className="App">
-      <h1>The Fast Section</h1>
+      <h1>The Fast Section in data storage</h1>
       <Cosmetics></Cosmetics>
       <Shoes></Shoes>
     </div>
